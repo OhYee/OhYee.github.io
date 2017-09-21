373dc5b0a02ee1f5f0be54d69398eeb39060f4f3
OhYee <oyohyee@oyohyee.com>
2017-09-21 23:14:01 +0800

    Site updated: 2017-09-21 23:12:15

---
+++ js/digit.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+	document.getElementById('site.tags.length').innerHTML='161'
+	document.getElementById('site.posts.length').innerHTML='462'
+	document.getElementById('site.categories.length').innerHTML='7'
+}
