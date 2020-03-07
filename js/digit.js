e21f526ee104cb4e1d6808a21ed917048aabebfb
OhYee <oyohyee@oyohyee.com>
2020-03-07 22:33:10 +0800

    jump to https://www.oyohyee.com

--- js/digit.js
+++
@@ -1,5 +0,0 @@
-window.onload=function(){
-	document.getElementById('site.tags.length').innerHTML='163'
-	document.getElementById('site.posts.length').innerHTML='475'
-	document.getElementById('site.categories.length').innerHTML='7'
-}
